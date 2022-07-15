4d090feefa7683aedc1ba073de24d0420d7e87c2
Brian D'Ostilio <briand787b@gmail.com>
2022-07-15 13:45:55 -0400

    adds e2e tests for ide svc

--- src/ui/e2e/cypress/e2e/spec.cy.js
+++ src/ui/e2e/cypress/e2e/spec.cy.js
@@ -1,105 +1,145 @@
-describe('item list', () => {
-  it('shows item list header', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
+describe('ide', () => {
+  describe('home page', () => {
+    it('should return to home page when clicking icon', () => {
+      // arrange
+      cy.visit('http://localhost:3000/')
+      
+      // arrange
+      cy.get('h1').click()
+  
+      // assert
+      cy.get('h2').should('include.text', 'Ready to dive in? Your New Virtual BoH: NCX');
+    })
 
-    // assert
-    cy.get('h2').contains('Items List');
+    it('should navigate to ide when "get started" button clicked"', () => {
+      // arrange
+      cy.visit('http://localhost:3000/')
+
+      // act
+      cy.get('a').contains('Get Started').click()
+
+      // assert
+      cy.url() === 'http://localhost:3000/ide' // TODO: This is definitely wrong
+      cy.get('h2').should('include.text', 'NCX')
+      cy.get('a').should('include.text', 'Edit Entities')
+      cy.get('a').should('include.text', 'Edit Business Rules')
+    })
   })
 
-  it('has 10 items', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
-
-    // assert
-    cy.get('#item-list').children().should('have.length', 10)
-  })
-})
-
-describe('total', ()=> {
-  it('shows total header', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
-
-    // assert
-    cy.get('h2').contains('Total');
+  describe('entities page', () => {
+    // placeholder
   })
 
-  it('has initial total of zero', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
-
-    // assert
-    cy.get('#total-list').children().should('have.length', 0)
-  })
-
-  it('total list contains 1 element when item clicked', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
-    const item = cy.get('#item-list').children().first();
-
-    // act
-    item.click()
-
-    // assert
-    cy.get('#total-list').children().should('have.length', 1)
-  })
-
-  it('total list contains 2 elements when two items clicked', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
-    const firstItem = cy.get('#item-list').children().first();
-    firstItem.click()
-    const secondItem = firstItem.next();
-    
-    // act
-    secondItem.click()
-    
-    // assert
-    cy.get('#total-list').children().should('have.length', 2)
+  describe('rules page', () => {
+    // placeholder
   })
 })
 
-describe('subtotal', ()=> {
-  it('shows subtotal header', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
+describe('pos', () => {
+  describe('item list', () => {
+    it('shows item list header', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
 
-    // assert
-    cy.get('h3').contains('Subtotal');
+      // assert
+      cy.get('h2').contains('Items List');
+    })
+
+    it('has 10 items', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
+
+      // assert
+      cy.get('#item-list').children().should('have.length', 10)
+    })
   })
 
-  it('has initial subtotal of zero', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
+  describe('total', () => {
+    it('shows total header', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
 
-    // assert
-    cy.get('#subtotal').should('include.text', '$0.00')
+      // assert
+      cy.get('h2').contains('Total');
+    })
+
+    it('has initial total of zero', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
+
+      // assert
+      cy.get('#total-list').children().should('have.length', 0)
+    })
+
+    it('total list contains 1 element when item clicked', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
+      const item = cy.get('#item-list').children().first();
+
+      // act
+      item.click()
+
+      // assert
+      cy.get('#total-list').children().should('have.length', 1)
+    })
+
+    it('total list contains 2 elements when two items clicked', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
+      const firstItem = cy.get('#item-list').children().first();
+      firstItem.click()
+      const secondItem = firstItem.next();
+
+      // act
+      secondItem.click()
+
+      // assert
+      cy.get('#total-list').children().should('have.length', 2)
+    })
   })
 
-  it('subtotal $10.00 when purchasing 1 item', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
-    const item = cy.get('#item-list').children().first();
+  describe('subtotal', () => {
+    it('shows subtotal header', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
 
-    // act
-    item.click()
+      // assert
+      cy.get('h3').contains('Subtotal');
+    })
 
-    // assert
-    cy.get('#subtotal').should('include.text', '$10.00')
-  })
+    it('has initial subtotal of zero', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
 
-  it('subtotal $20.00 when purchasing 2 items', () => {
-    // arrange
-    cy.visit('http://localhost:3001');
-    const firstItem = cy.get('#item-list').children().first();
-    firstItem.click()
-    const secondItem = firstItem.next()
+      // assert
+      cy.get('#subtotal').should('include.text', '$0.00')
+    })
 
-    // act
-    secondItem.click()
+    it('subtotal $10.00 when purchasing 1 item', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
+      const item = cy.get('#item-list').children().first();
 
-    // assert
-    cy.get('#subtotal').should('include.text', '$20.00')
+      // act
+      item.click()
+
+      // assert
+      cy.get('#subtotal').should('include.text', '$10.00')
+    })
+
+    it('subtotal $20.00 when purchasing 2 items', () => {
+      // arrange
+      cy.visit('http://localhost:3001');
+      const firstItem = cy.get('#item-list').children().first();
+      firstItem.click()
+      const secondItem = firstItem.next()
+
+      // act
+      secondItem.click()
+
+      // assert
+      cy.get('#subtotal').should('include.text', '$20.00')
+    })
   })
 })
+
